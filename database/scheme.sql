e076ed485651e1e01c25475bceb750aef39382d3
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-22 21:08:02 +0100

    créer la structure backend

---
+++ database/scheme.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE devbook;
+use devbook;
+
+CREATE TABLE users (
+    id AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
