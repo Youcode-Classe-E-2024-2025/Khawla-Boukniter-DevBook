25088f6253cb109152c5dacd3a95c071849d309e
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 01:24:00 +0100

    ajouter la route post /signup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const path = require('path');
+const User = require('../models/User');
 
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../..', 'pages/login.html'));
@@ -10,5 +10,22 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '../..', 'pages/signup.html'));
 });
+
+router.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+
+    const newUser = new User(null, username, email, password);
+
+    newUser.signup((err, rslt) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('erreur');
+
+        }
+        res.sendFile(path.join(__dirname, '../..', 'index.html'));
+    });
+
+
+})
 
 module.exports = router;
