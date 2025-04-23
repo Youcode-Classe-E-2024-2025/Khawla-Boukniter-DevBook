c16e49f127a82ed3f4eb56503455a501ebe1af9d
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 01:08:00 +0100

    activer le parsing json

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '..', 'assets')));
