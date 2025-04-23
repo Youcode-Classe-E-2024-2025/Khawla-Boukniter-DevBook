27863544b1c016d607353bf3ccbcd1ffc890b58f
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 01:35:00 +0100

    activer le parsing des données json et forms html

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-app.use(express.json());
 const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '..', 'assets')));
 app.get('/', (req, res) => {
