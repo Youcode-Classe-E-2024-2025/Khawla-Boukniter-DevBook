7492a0d4f6267d5d448de000855ec2c29e8089d2
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 01:45:00 +0100

    ajouter la methode login avec verification du mdp

--- backend/models/User.js
+++ backend/models/User.js
@@ -23,6 +23,33 @@ class User {
 
         console.log('connected');
     }
+
+    async login(passwordToCHeck, callback) {
+
+        const sql = 'SELECT * FROM users WHERE email = ?';
+        const value = [this.email];
+
+        db.query(sql, value, async (err, rslt) => {
+            if (err) return callback(err, null);
+
+            if (rslt.length === 0) {
+                return callback(null, { success: false, message: "user introuvable" });
+            }
+
+            const user = rslt[0];
+
+            const match = await bcrypt.compare(passwordToCHeck, user.password);
+
+            if (!match) {
+                return callback(null, { success: false, message: "mdp incorrect" });
+            }
+
+            callback(null, { success: true, user });
+        })
+
+        console.log('connected');
+
+    }
 }
 
 module.exports = User;
