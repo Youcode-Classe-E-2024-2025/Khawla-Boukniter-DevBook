2a92f339630fea84e3346e6e843e869352988afe
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 15:30:00 +0100

    add error handling

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch('/categories')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
         .then(data => {
             const container = document.getElementById('categoryList');
             container.innerHTML = '';
--- assets/js/livres.js
+++ assets/js/livres.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch('/books')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
         .then(data => {
             const container = document.querySelector('.container');
             container.innerHTML = '';
