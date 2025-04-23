3ea0bc6bde07e2a5bd9e2ff73ce6b57b28c62668
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-23 02:49:00 +0100

    ajouter les routes pour les categories

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const CategoryController = require('../controllers/CategoryController');
 
-router.get('/', (req, res) => {
-    res.send('categories route');
-});
+router.get('/', CategoryController.getAll);
+
+router.get('/:id', CategoryController.getById);
+
+router.post('/', CategoryController.create);
+
+router.put('/:id', CategoryController.update);
+
+router.delete('/:id', CategoryController.delete);
 
 module.exports = router;
